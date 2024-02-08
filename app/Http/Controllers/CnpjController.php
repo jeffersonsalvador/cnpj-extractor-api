0368788cb21b7160b5b63ae3bb097dc6bae4a492
Jefferson Costa <jeffersonsalvador@gmail.com>
2024-02-08 21:08:34 +0100

    Cnpj model

--- app/Http/Controllers/CnpjController.php
+++ app/Http/Controllers/CnpjController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author    Jefferson Costa
- * @copyright 2024 GAIA AG, Hamburg
+ * @copyright 2024, Malaga
  * @package   cnpj-extractor-api
  *
  * Created using PhpStorm at 07.02.24 20:43
@@ -9,9 +9,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CnpjResource;
+use App\Models\Cnpj;
+use Illuminate\Http\JsonResponse;
+
 class CnpjController extends Controller
 {
-    public function show() {
+    public function show($cnpj): JsonResponse
+    {
+        $query = Cnpj::query()->where('çnpj', $cnpj);
+        $cacheName = "";
 
+        return CnpjResource::collection();
     }
 }
---
+++ app/Models/Cnpj.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author    Jefferson Costa
+ * @copyright 2024, Malaga
+ * @package   cnpj-extractor-api
+ *
+ * Created using PhpStorm at 08.02.24 21:00
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cnpj extends Model
+{
+
+}
